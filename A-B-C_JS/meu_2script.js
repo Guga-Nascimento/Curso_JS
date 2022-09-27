8dc85dceeedbe81cd406710e30ecd30fd7ec096e
Guga-Nascimento <guganascimento001@gmail.com>
2022-09-27 05:04:56 -0300

    Salvando a primeira pasta do Curso_JS

---
+++ A-B-C_JS/meu_2script.js
@@ -0,0 +1,5 @@
+var nome = prompt('digite seu nome?')
+var idade = prompt('digite a sua idade?')
+
+document.write('<h1> Olá ' + nome + ', tudo bem? <br> Seu cadastro foi aprovado com sucesso. <br> Vimos que sua idade é '
++ idade+ ' Anos. E agradecemos a preferência.' )
---
+++ A-B-C_JS/meu_script.js
@@ -0,0 +1,12 @@
+alert('Olá, seja bem vindo ao curso!!!')
+document.getElementById('nome').value = 'Interessante saber que o ideal na verdade é colocar o script após o comando'
+
+//string
+var texto = 'Curso de JavaScript, esse comando foi executado com o document.write direto do js para a tela do usuário.'
+//number
+var num_int = -7
+var num_float = 5.5
+//boolean
+var teste = true 
+
+document.write(texto)
