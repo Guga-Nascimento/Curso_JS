40dfc329e3f8e1ec0db1063a3f5e387bd3dbe7c3
Guga-Nascimento <guganascimento001@gmail.com>
2022-09-27 21:26:41 -0300

    Entendendo if else

---
+++ if-else.js
@@ -0,0 +1,11 @@
+if (true){
+    document.write('Entrou dentro do bloco if verdadeiro <br>')
+}else {
+    document.write('Entrou dentro do bloco if falso. <br>')
+}
+
+if (2 != 2){
+    document.write('Entrou dentro do bloco if verdadeiro')
+}else {
+    document.write('Entrou dentro do bloco if falso')
+}
---
+++ if-else2.js
@@ -0,0 +1,9 @@
+var nota = prompt ('Digite a nota do aluno:')
+var media = 7
+
+if(nota >= 7){
+    document.write('<h1>Aprovado</h1>')
+    
+}else{
+    document.write('<strong>Reprovado</strong>')
+}
